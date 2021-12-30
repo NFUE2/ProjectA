694068d4c6ba5e9ac3cd7733e9bbf8dc4a422709
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2021-12-30 12:17:14 +0900

    ETG

---
+++ ETG2021.12.29/Assets/Script/PlayerFire.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerFire : MonoBehaviour
+{
+    GameObject _weapon;
+    GameObject _item;
+
+    Image _image;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        //Cursor.visible = false; //커서를 안보이게 해줍니다.
+        Vector3 dir = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        dir = _image.transform.position;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(_weapon.GetComponentInChildren<GameObject>());
+            Vector3 shoot = this.transform.position - dir;
+            
+        }
+
+
+    }
+}
---
+++ ETG2021.12.29/Assets/Script/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    float _speed = 5.0f;
+    Rigidbody _rb;
+    bool rolling = false;
+
+    void Start()
+    {
+        _rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+        float h = Input.GetAxis("Horizontal"); //좌우
+        float v = Input.GetAxis("Vertical"); //위아래
+
+        Vector3 dir = h * Vector3.right + v * Vector3.up;
+        if(dir == Vector3.zero)
+        {
+            
+        }
+        
+        this.transform.position += dir * _speed * Time.deltaTime;
+
+        if(Input.GetMouseButtonDown(1) && rolling == false && dir !=Vector3.zero)
+        {
+            rolling = true;
+            _rb.AddForce(dir * _speed, ForceMode.Impulse);
+        }
+        rolling = false;
+    }
+}
