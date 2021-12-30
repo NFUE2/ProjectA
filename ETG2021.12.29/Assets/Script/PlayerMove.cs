0ab717e2ef853e1318f1a40b683926156f7ddc77
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2021-12-30 17:51:08 +0900

    ETG

--- ETG2021.12.29/Assets/Script/PlayerMove.cs
+++ ETG2021.12.29/Assets/Script/PlayerMove.cs
@@ -5,33 +5,98 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     float _speed = 5.0f;
+    Animator _ani;
     Rigidbody _rb;
-    bool rolling = false;
 
+    enum PlayerState
+    {
+        Idle,
+        Walk,
+        Rolling,
+        Up,
+        Down,
+        Left,
+        Right,
+        LeftUp,
+        RightUp,
+        LeftDown,
+        RightDown
+    }
+
+    PlayerState state;
     void Start()
     {
         _rb = this.GetComponent<Rigidbody>();
+        _ani = this.GetComponent<Animator>();
+        state = PlayerState.Idle;
     }
 
     void Update()
     {
-        
-        float h = Input.GetAxis("Horizontal"); //좌우
         float v = Input.GetAxis("Vertical"); //위아래
+        float h = Input.GetAxis("Horizontal"); //좌우
 
         Vector3 dir = h * Vector3.right + v * Vector3.up;
-        if(dir == Vector3.zero)
+
+        if(v > 0) //위로 올라갈때
         {
-            
+            if(h == 0)
+            {
+                state = PlayerState.Up;
+            }
+            else if (h > 0)
+            {
+                state = PlayerState.RightUp;
+            }
+            else if (h < 0)
+            {
+                state = PlayerState.LeftUp;
+            }
+        }
+        else if(v <0) //아래로 내려갈때
+        {
+            if (h == 0)
+            {
+                state = PlayerState.Down;
+            }
+            else if (h > 0)
+            {
+                state = PlayerState.RightDown;
+            }
+            else if (h < 0)
+            {
+                state = PlayerState.LeftDown;
+            }
+        }
+        else if(v==0)
+        {
+            if(h > 0)
+            {
+                state = PlayerState.Right;
+            }
+            else if (h < 0)
+            {
+                state = PlayerState.Left;
+            }
         }
         
+        if(dir == Vector3.zero)
+        {
+            state = PlayerState.Idle;
+        }
+
+        Debug.Log(state);
+
+
         this.transform.position += dir * _speed * Time.deltaTime;
 
-        if(Input.GetMouseButtonDown(1) && rolling == false && dir !=Vector3.zero)
+        if(Input.GetMouseButtonDown(1))
         {
-            rolling = true;
             _rb.AddForce(dir * _speed, ForceMode.Impulse);
+
+            _rb.velocity = Vector3.zero; //일정 시간 뒤 작동하게 해야할듯
+            Debug.Log("구른다");
         }
-        rolling = false;
     }
+    
 }
